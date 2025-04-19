1901dde4761ff211ca152740b6e4e8b7f7fe0ae5
Fadi Hamwi <fadihamwi2001@gmail.com>
2025-04-19 20:58:43 +0000

    Finished Limits.c

--- hw-intro/limits.c
+++ hw-intro/limits.c
@@ -3,8 +3,32 @@
 
 int main() {
     struct rlimit lim;
-    printf("stack size: %ld\n", 0L);
-    printf("process limit: %ld\n", 0L);
-    printf("max file descriptors: %ld\n", 0L);
+    if(getrlimit(RLIMIT_STACK, &lim) == -1) {
+        fprintf(stderr, "Couldn't use softlimits of stack.\n");
+    }
+    if(lim.rlim_cur == RLIM_INFINITY) {
+        printf("Stack size of the process is unlimited.\n");
+    }else {
+        printf("stack size: %lud\n", (unsigned long)lim.rlim_cur);
+    }
+
+    if(getrlimit(RLIMIT_AS, &lim) == -1) {
+        fprintf(stderr, "couldn't set the soft limit of process address space.\n");
+    }
+    if(lim.rlim_cur == RLIM_INFINITY) {
+        printf("Process address is unlimited.\n");
+    }else {
+        printf("process limit: %lud\n", (unsigned long)lim.rlim_cur);
+    }
+
+    if(getrlimit(RLIMIT_NOFILE, &lim) == -1) {
+        fprintf(stderr, "Couldn't set the max number of file descriptors.\n");
+    }
+
+    if(lim.rlim_cur == RLIM_INFINITY) {
+        printf("Number of file descriptors is unlimited.\n");
+    }else {
+        printf("max file descriptors: %lud\n", (unsigned long)lim.rlim_cur);
+    }
     return 0;
 }
